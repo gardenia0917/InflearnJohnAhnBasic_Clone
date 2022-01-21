9d7ef5d8bcd09bc9bd73490a9531578a7ac62ebc
gardenia1109 <gardenia1109@naver.com>
2022-01-22 03:11:31 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+// bodyParser가 서버에서 분석해서 가져올수 있게 해주는 것
+
+//application/x-www-form-urlencoded를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://gardenia1109:ruxia10173704@cluster0.o8fyt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log("err"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 db에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
